a9d0333f9e0b5825102e38dac2ae2ab792ecf380
zhaozongchang <xingyingsuifeng@gmail.com>
2018-03-10 09:43:17 +0800

    def cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,9 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    redirect_to :back
-    flash[:notice] = "测试加入购车"
-  end
+     @product = Product.find(params[:id])
+     current_cart.add_product_to_cart(@product)
+     redirect_to :back
+     flash[:notice] = "加入购物车成功"
+   end
 end
